3264a70875e33bea152adddb17ba49442ba9f908
endaokhai <89455047+endaokhai@users.noreply.github.com>
2022-06-26 21:45:08 +0800

    asdas
    sadda

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  console.log("niama pornhub i am here");
-  res.end('Hello World!');
-}).listen(3000);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
